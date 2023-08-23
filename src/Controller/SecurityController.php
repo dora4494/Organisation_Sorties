c9dce174f8dab89a32be615f59e43a37a438c568
qmasson2023 <quentmasson@gmail.com>
2023-08-23 16:38:04 +0200

    ...

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,14 +2,13 @@
 
 
 namespace App\Controller;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 
 
 class SecurityController extends AbstractController
@@ -18,36 +17,18 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
 
-        /*$form = $this->createFormBuilder()
-            ->add('email', TextType::class,[
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'L\'adresse email est requise.',
-                    ]),
-                    new Email([
-                        'message' => 'L\'adresse email "{{ value }}" n\'est pas valide.',
-                    ]),
-                ],
-            ])
-            ->add('mot de passe', TextType::class)
-            ->add('valider', SubmitType::class)
-            ->getForm();
-
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $data = $form->getData();
-            $session->set('user', $data);
-
-        }*/
-
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
-        return $this->render(':security:login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('accueil');
+        }
+        else{
+            return $this->render('security/login.html.twig', [
+                'last_username' => $lastUsername,
+                'error' => $error,
+            ]);
+        }
     }
 
     #[Route(path: '/logout', name: 'app_logout')]

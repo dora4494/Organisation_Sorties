03961aca835eca40e7974ac46ab950ee2223820d
qmasson2023 <quentmasson@gmail.com>
2023-08-24 16:04:37 +0200

    ...

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -14,7 +15,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, UserPasswordHasherInterface $userPasswordHasher): Response
     {
 
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -24,6 +25,7 @@ class SecurityController extends AbstractController
             return $this->redirectToRoute('accueil');
         }
         else{
+            dump($error);
             return $this->render('security/login.html.twig', [
                 'user' => $this->getUser(),
                 'last_username' => $lastUsername,
@@ -32,7 +34,7 @@ class SecurityController extends AbstractController
         }
     }
 
-    #[Route(path: '/login', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
 
--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -28,13 +28,13 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
 
     public function authenticate(Request $request): Passport
     {
-        $username = $request->request->get('username', '');
+        $username = $request->request->get('mail', '');
 
         $request->getSession()->set(Security::LAST_USERNAME, $username);
-
         return new Passport(
             new UserBadge($username),
-            new PasswordCredentials($request->request->get('password', '')),
+
+            new PasswordCredentials($request->request->get('motDePasse', '')),
             [
                 new CsrfTokenBadge('authenticate', $request->request->get('_csrf_token')),
                 new RememberMeBadge(),

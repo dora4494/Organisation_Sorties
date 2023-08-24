32cf1ab2f04265f388aa464e35ee213e093e2d73
Michaud Nathanael <nath.michaud@hotmail.fr>
2023-08-24 16:40:22 +0200

    Résolution des problèmes de merge

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+
 ];

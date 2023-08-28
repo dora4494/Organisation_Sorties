7c204d90bf5550aa07f55054d59da5a635d66136
Pandora Pierrot <pandora.pierrot@gmail.com>
2023-08-28 13:59:40 +0200

    Page détails - lien vers le profil des inscrits

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Repository\ParticipantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,4 +17,23 @@ class ProfileController extends AbstractController
             'controller_name' => 'ProfileController',
         ]);
     }
+
+
+    // Lien vers le profil des personnes inscrites à une sortie
+    #[Route('/profile/{id}', name: 'app_profile_inscrit')]
+    public function profilInscrit(
+        int $id,
+        ParticipantRepository $participantRepository
+    ): Response
+    {
+        // Pour récupérer l'id du participant inscrit
+          $participant = $participantRepository->find($id);
+
+        return $this->render('profile/profil_inscrit.html.twig', [
+            'participant' => $participant
+        ]);
+    }
+
+
+
 }
